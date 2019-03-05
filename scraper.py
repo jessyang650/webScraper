fe69378bac9dda18549efcc2562b0c0e2dae2eae
Jess Yang <jessyang650@users.noreply.github.com>
2019-03-05 15:27:50 -0800

    created dump onto csv file

--- scraper.py
+++ scraper.py
@@ -1,12 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
-source = requests.get('https://woodworkingformeremortals.com/plans/').text
+html = requests.get('https://woodworkingformeremortals.com/plans/').text
 
-soup = BeautifulSoup(source, 'lxml')
+soup = BeautifulSoup(html, 'lxml')
+
+csv_file = open('wood_working_plans.csv', 'w')
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Project', 'Plans'])
 
 for columns in soup.find_all('ul'):
     project = columns.li.a.text
-    pdf_links = columns.li.a
+    pdf_links = columns.li.a.get('href')
+
     print(project)
-    print(pdf_links.get('href'))
+    print(pdf_links)
+
+    csv_writer.writerow([project, pdf_links])
+
+csv_file.close()
