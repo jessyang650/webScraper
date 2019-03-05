adb413679e0d72cf4aff3e33e5684c7221070f87
Jess Yang <jessyang650@users.noreply.github.com>
2019-03-05 12:13:00 -0800

    modified logic to show project name and links

--- scraper.py
+++ scraper.py
@@ -6,5 +6,7 @@ source = requests.get('https://woodworkingformeremortals.com/plans/').text
 soup = BeautifulSoup(source, 'lxml')
 
 for columns in soup.find_all('ul'):
-    plans = columns.li.a
-    print(plans)
+    project = columns.li.a.text
+    pdf_links = columns.li.a
+    print(project)
+    print(pdf_links.get('href'))
